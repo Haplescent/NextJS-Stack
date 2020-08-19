bb0b363eec1622e1a9fcf8fd90dcb00f54ec2dca
John Merritt <johnmerritt95@gmail.com>
2020-08-19 11:02:39 -0700

    added _app.js and _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -1,4 +1,8 @@
+/* eslint-disable react/jsx-props-no-spreading */
+/* eslint-disable prettier/prettier */
+import React from 'react';
 import Document, { Head, Html, Main, NextScript } from 'next/document';
+import { ServerStyleSheets } from '@material-ui/styles';
 
 class MyDocument extends Document {
   render() {
@@ -6,17 +10,14 @@ class MyDocument extends Document {
       <Html lang="en">
         <Head>
           <meta charSet="utf-8" />
-          {/* // tells browser that content is UTF-8 encoded */}
           <meta
-            //   {/* // eslint-disable-line */}
             name="viewport"
             content="width=device-width, initial-scale=1.0"
           />
-          {/* // sets page width to screen width, sets initial zoom */}
           <meta name="google" content="notranslate" />
           {/* // tell google to not show translate modals */}
           <meta name="theme-color" content="#1976D2" />
-          {/* // specifies color of browser on mobile device */}
+
           <link
             rel="shortcut icon"
             href="https://storage.googleapis.com/builderbook/favicon32.png"
@@ -26,7 +27,6 @@ class MyDocument extends Document {
             href="https://fonts.googleapis.com/css?family=Muli:300,400:latin"
           />
           <link
-            //   {/* // eslint-disable-line */}
             rel="stylesheet"
             href="https://fonts.googleapis.com/icon?family=Material+Icons"
           />
@@ -35,42 +35,39 @@ class MyDocument extends Document {
             href="https://storage.googleapis.com/builderbook/nprogress.min.css"
           />
           <link
-            //   {/* // eslint-disable-line */}
             rel="stylesheet"
             href="https://storage.googleapis.com/builderbook/vs.min.css"
           />
 
           <style>
             {`
-   a, a:focus {
-     font-weight: 400;
-     color: #1565C0;
-     text-decoration: none;
-     outline: none
-   }
-   a:hover, button:hover {
-     opacity: 0.75;
-     cursor: pointer
-   }
-   blockquote {
-     padding: 0 1em;
-     color: #555;
-     border-left: 0.25em solid #dfe2e5;
-   }
-   pre {
-     display:block;
-     overflow-x:auto;
-     padding:0.5em;
-     background:#FFF;
-     color: #000;
-     border: 1px solid #ddd;
-   }
-   code {
-     font-size: 14px;
-     background: #FFF;
-     padding: 3px 5px;
-   }
- `}
+              a, a:focus {
+                font-weight: 400;
+                color: #1565C0;
+                text-decoration: none;
+                outline: none
+              }
+              a:hover, button:hover {
+                opacity: 0.75;
+                cursor: pointer
+              }
+              blockquote {
+                padding: 0 1em;
+                color: #555;
+                border-left: 0.25em solid #dfe2e5;
+              }
+              pre {
+                display:block;
+                overflow-x:auto;
+                padding:0.5em;
+                background:#FFF;
+                color: #000;
+                border: 1px solid #ddd;
+              }
+              code {
+                font-size: 14px;
+              }
+            `}
           </style>
         </Head>
         <body
@@ -90,5 +87,51 @@ class MyDocument extends Document {
     );
   }
 }
+
+MyDocument.getInitialProps = async ctx => {
+  // Resolution order
+  //
+  // On the server:
+  // 1. app.getInitialProps
+  // 2. page.getInitialProps
+  // 3. document.getInitialProps
+  // 4. app.render
+  // 5. page.render
+  // 6. document.render
+  //
+  // On the server with error:
+  // 1. document.getInitialProps
+  // 2. app.render
+  // 3. page.render
+  // 4. document.render
+  //
+  // On the client
+  // 1. app.getInitialProps
+  // 2. page.getInitialProps
+  // 3. app.render
+  // 4. page.render
+
+  // Render app and page and get the context of the page with collected side effects.
+  const sheets = new ServerStyleSheets();
+  const originalRenderPage = ctx.renderPage;
+
+  ctx.renderPage = () =>
+    originalRenderPage({
+      enhanceApp: App => props => sheets.collect(<App {...props} />),
+    });
+
+  const initialProps = await Document.getInitialProps(ctx);
+
+  return {
+    ...initialProps,
+    // Styles fragment is rendered after the app and page rendering finish.
+    styles: (
+      <>
+        {initialProps.styles}
+        {sheets.getStyleElement()}
+      </>
+    ),
+  };
+};
 
 export default MyDocument;
--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,18 @@
+/* eslint-disable prettier/prettier */
 import Head from 'next/head';
-import Header from '../components/Header';
+import Button from '@material-ui/core/Button';
 
 const Index = () => (
   <div style={{ padding: '10px 45px' }}>
     <Head>
       <title>Index page</title>
       <meta
-        //   {/* // eslint-disable-line */}
         name="description"
         content="This is the description of the Index page"
       />
     </Head>
-    <Header />
     <p>Content on Index page</p>
+    <Button variant="contained">MUI button</Button>
   </div>
 );
 
